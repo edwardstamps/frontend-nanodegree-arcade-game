978fa17f8257c47df2563b66c44dc36fd61073e7
edwardstamps <wedwardstamps@Edwards-MacBook-Pro.local>
2015-01-29 17:28:05 -0700

    working demo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
+
+
+
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
+    this.speed = Math.random() * (400-100)+100;
+    this.x = x;
+    this.y = Math.random()*(220-42)+42;
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,9 +17,34 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 }
 
+
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    /* if (player === (202,404)) {
+        this.speed=0;
+    } */
+    if (this.x < 606) {
+        this.x += this.speed * dt;
+
+    }else{
+        this.x=0
+        this.y = Math.random()*(220-42)+42;
+    }
+
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+        Player.prototype.reset();
+        }
+    }
+
+
+
+
+    
+
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -34,6 +68,88 @@ Enemy.prototype.render = function() {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+
+var Player = function() {
+    
+    this.x = 202;
+    this.y = 404;
+
+    this.guy = 'images/char-boy.png';
+}
+
+Player.prototype.reset = function() {
+
+    player.x = 202;
+    player.y = 404;
+
+}
+
+
+Player.prototype.update = function() {
+    if (this.Key === 'left') {
+        this.x = this.x - 101;
+
+    }
+
+    if (this.Key === 'left' && this.x === -101) {
+        this.x = 505;
+
+    } 
+
+     if (this.Key === 'right') {
+        this.x = this.x + 101;
+
+    }
+
+     if (this.Key === 'right' && this.x === 606) {
+        this.x = 0;
+
+    }
+
+     if (this.Key === 'up') {
+        this.y = this.y - 83;
+
+    }
+
+     if (this.Key === 'down') {
+        this.y = this.y + 83;
+
+    }
+
+    this.Key = null;
+
+    if (this.y < 60){
+        this.reset();
+    }
+
+    
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.guy), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(e){
+    this.Key = e;    
+}
+
+
+
+var allEnemies = [];
+
+
+
+var makeEnemies = function(){
+    allEnemies.push(new Enemy(-50));
+    allEnemies.push(new Enemy(-200));
+    allEnemies.push(new Enemy(-300));
+    
+}();
+
+
+var player = new Player();
+    
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
